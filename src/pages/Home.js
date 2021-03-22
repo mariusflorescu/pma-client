2921c73c0a7e7f1329d701ceb69107132fe8a598
Marius Florescu <mariusflorescu23@icloud.com>
2021-03-22 10:38:21 +0200

    Created useRouter custom hook (Nextjs like)

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,12 @@
 import React from 'react'
+import useRouter from '../utils/useRouter'
+
 
 const Home = () => {
+  const router = useRouter();
+
+  console.log(router);
+
   return (
     <div>
       Hello from home page!
