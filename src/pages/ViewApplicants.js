1b3ff445fddee8cc1b4a0c2f1ded4b530a237c52
Marius Florescu <mariusflorescu23@icloud.com>
2021-05-12 23:57:57 +0300

    Added -> This project has no applicants

--- src/pages/ViewApplicants.js
+++ src/pages/ViewApplicants.js
@@ -31,43 +31,46 @@ function ViewApplicants() {
     <div className="flex flex-col">
       <div className="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
         <div className="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
-          <div className="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
-            <table className="min-w-full divide-y divide-gray-200">
-              <thead className="bg-gray-50">
-                <tr>
-                  <th
-                    scope="col"
-                    className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-                  >
-                    Student
-                  </th>
-                </tr>
-              </thead>
-              <tbody className="bg-white divide-y divide-gray-200">
-                {applicants &&
-                  applicants.length &&
+          {applicants && applicants.length ? (
+              <div className="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+                <table className="min-w-full divide-y divide-gray-200">
+                  <thead className="bg-gray-50">
+                  <tr>
+                    <th
+                        scope="col"
+                        className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                    >
+                      Student
+                    </th>
+                  </tr>
+                  </thead>
+                  <tbody className="bg-white divide-y divide-gray-200">
+                  {applicants &&
                   applicants.map((applicant) => (
-                    <tr key={applicant.id}>
-                      <td className="px-6 py-4 whitespace-nowrap">
-                        <div className="flex items-center">
-                          <div className="flex-shrink-0 h-10 w-10">
-                            <img
-                              className="h-10 w-10 rounded-full"
-                              src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60"
-                              alt=""
-                            />
-                          </div>
-                          <div className="ml-4 font-medium text-sm">
-                            {applicant.student.firstname +
+                      <tr key={applicant.id}>
+                        <td className="px-6 py-4 whitespace-nowrap">
+                          <div className="flex items-center">
+                            <div className="flex-shrink-0 h-10 w-10">
+                              <img
+                                  className="h-10 w-10 rounded-full"
+                                  src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60"
+                                  alt=""
+                              />
+                            </div>
+                            <div className="ml-4 font-medium text-sm">
+                              {applicant.student.firstname +
                               applicant.student.lastname}
+                            </div>
                           </div>
-                        </div>
-                      </td>
-                    </tr>
+                        </td>
+                      </tr>
                   ))}
-              </tbody>
-            </table>
-          </div>
+                  </tbody>
+                </table>
+              </div>
+          ):(
+              <span className="font-semibold text-medium text-gray-600">There are no applicants</span>
+          )}
           <div className="flex justify-center pt-6 ">
             <button
               className="px-3 py-1 leading-5 text-xs font-semibold rounded-full bg-green-100 text-green-800 focus:outline-none cursor:pointer"
