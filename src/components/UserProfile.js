8b5e9a9d05db359954b75b6df0b593c9d523074c
Marius Florescu <mariusflorescu23@icloud.com>
2021-05-15 01:26:36 +0300

    Finished updating description

--- src/components/UserProfile.js
+++ src/components/UserProfile.js
@@ -1,9 +1,12 @@
 import React from "react";
 import { useAuthState } from "../utils/authContext";
+import axios from "axios";
 
 const UserProfile = () => {
   const { user } = useAuthState();
   const [namePic, setNamePic] = React.useState("");
+  const [description,setDescription] = React.useState("");
+  const [open,setOpen] = React.useState(false);
 
   React.useEffect(() => {
     if (!user) {
@@ -21,6 +24,42 @@ const UserProfile = () => {
     }
   }, [user]);
 
+  React.useEffect(() => {
+    if(user && user.data){
+      setDescription(user.data.description);
+    }
+  },[user]);
+
+  const handleSaveStudent = (e) => {
+    e.preventDefault();
+
+    const id = user.data.id;
+
+    axios.put('/users/student/description',{id,description})
+        .then((res) => {
+          console.log(res.data);
+          setOpen(false);
+        })
+        .catch((err) => {
+          console.log(err);
+        })
+  }
+
+  const handleSaveCompany = (e) => {
+    e.preventDefault();
+
+    const id = user.data.id;
+
+    axios.put('/users/company/description',{id,description})
+        .then((res) => {
+          console.log(res.data);
+          setOpen(false);
+        })
+        .catch((err) => {
+          console.log(err);
+        })
+  }
+
   return (
     <div className="flex-shrink-0 w-64 py-1 pr-1">
       <div className="h-full p-2 bg-gray-50 rounded-r-xl">
@@ -28,6 +67,9 @@ const UserProfile = () => {
           <span className="w-28 h-28 bg-green-400 rounded-full flex items-center justify-center text-5xl text-white ">
             {user && namePic}
           </span>
+          <svg xmlns="http://www.w3.org/2000/svg" className="ml-3 p-2 rounded-full w-10 h-10 hover:bg-gray-200 transition duration-100 cursor-pointer" fill="none" viewBox="0 0 24 24" stroke="currentColor" onClick={(e) => {e.preventDefault(); setOpen(!open)}}>
+            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z" />
+          </svg>
         </div>
         {user && user.type === "STUDENT" ? (
           <>
@@ -47,14 +89,28 @@ const UserProfile = () => {
                 {user && user.data ? user.data.github_username : ""}
               </span>
             </div>
-            <div className="flex-col justify-center pt-4">
+            <div className="flex-col justify-center items-center pt-4">
               <label className="flex justify-center font-normal text-sm">
                 Who am I?
               </label>
-              <textarea
-                placeholder="Add description here..."
-                className="rounded-lg mt-2 pl-2 pt-1 pb-1 pr-2 w-full h-20 text-gray-600 bg-gray-50 border-2 border-gray-400 border-opacity-25 focus:outline-none"
-              />
+              {user && user.data && !open &&
+              (<span className="flex justify-center text-sm text-gray-600 pt-4">{description}</span>)
+              }
+              {user && user.data && open && (
+                  <React.Fragment>
+                    <textarea
+                        placeholder="Add description here..."
+                        value={description ? description : ""}
+                        onChange={(e) => setDescription(e.target.value)}
+                        className="rounded-lg mt-2 pl-2 pt-1 pb-1 pr-2 w-full h-20 text-gray-600 bg-gray-50 border-2 border-gray-400 border-opacity-25 focus:outline-none"
+                    />
+                    <button
+                        className="w-full flex justify-center px-3 py-2 mt-2 space-x-2 text-white transition duration-200 transform bg-green-500 rounded-full hover:scale-90 hover:bg-green-400 focus:outline-none"
+                        onClick={handleSaveStudent}>
+                      Save
+                    </button>
+                  </React.Fragment>
+              )}
             </div>
           </>
         ) : (
@@ -73,10 +129,24 @@ const UserProfile = () => {
               <label className="flex justify-center font-normal text-sm">
                 More about us
               </label>
-              <textarea
-                placeholder="Add description here..."
-                className="rounded-lg mt-2 pl-2 pt-1 pb-1 pr-2 w-full h-20 text-gray-600 bg-gray-50 border-2 border-gray-400 border-opacity-25 focus:outline-none"
-              />
+              {user && user.data && !open &&
+              (<span className="flex justify-center text-sm text-gray-600 pt-4">{description}</span>)
+              }
+              {user && user.data && open && (
+                  <React.Fragment>
+                    <textarea
+                        placeholder="Add description here..."
+                        value={description ? description : ""}
+                        onChange={(e) => setDescription(e.target.value)}
+                        className="rounded-lg mt-2 pl-2 pt-1 pb-1 pr-2 w-full h-20 text-gray-600 bg-gray-50 border-2 border-gray-400 border-opacity-25 focus:outline-none"
+                    />
+                    <button
+                        className="w-full flex justify-center px-3 py-2 mt-2 space-x-2 text-white transition duration-200 transform bg-green-500 rounded-full hover:scale-90 hover:bg-green-400 focus:outline-none"
+                        onClick={handleSaveCompany}>
+                      Save
+                    </button>
+                  </React.Fragment>
+              )}
             </div>
           </>
         )}
