2bfc11c5c08a70fa1a83b622a85821f79d1355f3
CernaVictor <vic.cerna01@gmail.com>
2021-05-15 18:13:34 +0300

    Added view tasks in my projects table

---
+++ src/pages/ViewTasks.js
@@ -0,0 +1,10 @@
+import axios from "axios";
+import React from "react";
+import useRouter from "../utils/useRouter";
+import { useParams } from "react-router-dom";
+
+function ViewTasks() {
+  return <div>Hello from view tasks page</div>;
+}
+
+export default ViewTasks;
