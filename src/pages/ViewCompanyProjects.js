3c6cce652410c61c803c3528e8c055b58274e107
CernaVictor <vic.cerna01@gmail.com>
2021-05-15 15:53:13 +0300

    Added Edit and Delete icons

--- src/pages/ViewCompanyProjects.js
+++ src/pages/ViewCompanyProjects.js
@@ -114,17 +114,44 @@ function ViewCompanyProjects() {
                           )}
                         </button>
                       </td>
-                      <td className="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
-                        <button className="transition duration-100 text-green-600 hover:text-green-900 cursor-pointer">
-                          <Link to={`/project/info/${project.id}`}>EDIT</Link>
+                      <td className="px-6 py-4 whitespace-nowrap text-right text-sm font-medium"></td>
+                      <td className="px-6 py-4 whitespace-nowrap text-right text-sm font-medium space-x-1">
+                        <button className="transition duration-100 text-gray-600 hover:text-green-900 cursor-pointer">
+                          <Link to={`/project/info/${project.id}`}>
+                            <svg
+                              xmlns="http://www.w3.org/2000/svg"
+                              class="h-6 w-6"
+                              fill="none"
+                              viewBox="0 0 24 24"
+                              stroke="currentColor"
+                            >
+                              <path
+                                stroke-linecap="round"
+                                stroke-linejoin="round"
+                                stroke-width="2"
+                                d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z"
+                              />
+                            </svg>
+                          </Link>
                         </button>
-                      </td>
-                      <td className="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
                         <button
-                          className="transition duration-100 text-green-600 hover:text-green-900 cursor-pointer"
+                          className="transition duration-100 text-gr-600 hover:text-green-900 cursor-pointer"
                           onClick={() => handleDeleteProject(project.id)}
                         >
-                          DEL
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            class="h-6 w-6"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            stroke="currentColor"
+                          >
+                            <path
+                              stroke-linecap="round"
+                              stroke-linejoin="round"
+                              stroke-width="2"
+                              d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
+                            />
+                          </svg>
                         </button>
                       </td>
                     </tr>
